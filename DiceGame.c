ac847309df9520f29f69a1c701c9920abc6b80d5
s1300238 <119606073+shima-kawa@users.noreply.github.com>
2023-06-09 11:19:50 +0900

    Create DiceGame.c
    Dice game: initial revision

---
+++ DiceGame.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int i, sum=0;
+  int result[2];
+
+  /* init randam function by time */
+  srand((unsigned int)time(NULL));
+  
+  printf("Rolling dice...\n");
+  for(i=0;i<2;i++){
+    sum += result[i] = rand()%6+1;
+    printf("Die %d: %d\n", i+1, result[i]);
+  }
+  printf("Total value: %d\n", sum);
+}
